caa3a10b3c7afa5e6b465f25eaaed0471fda8e18
Jaroslav Ban <jaroban@yahoo.com>
2018-12-06 20:43:22 +0100

    Added images

---
+++ Website_public/update.bat
@@ -0,0 +1,19 @@
+copy program.html program.aspx
+perl -x %0 & pause & exit
+#!perl
+use strict;
+use warnings;
+use File::Slurper qw(read_text write_text);
+
+my $file = read_text('index.html');
+$file = '<%@ Master Language="C#" AutoEventWireup="true" CodeFile="MasterPageIndex.master.cs" Inherits="MasterPageIndex" %>' . 
+	"\n" . $file;
+$file =~ s{<!-- <asp:ContentPlaceHolder id="Registration(Head|Body)" runat="server"></asp:ContentPlaceHolder> -->}{<asp:ContentPlaceHolder id="Registration$1" runat="server"></asp:ContentPlaceHolder>}g;
+$file =~ s/<head>/<head runat="server">/g;
+$file =~ s/program\.html/program.aspx/g;
+write_text('MasterPageIndex.master', $file);
+
+# $file = read_text('Website_public/program.html');
+# write_text('Website_public/program.aspx', $file);
+
+print "Success!\n";
